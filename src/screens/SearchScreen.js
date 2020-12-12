14112706a409b6946ed5ba603f08fbfffb19a689
Pedro <pedro.nunes@southsystem.com.br>
2020-12-12 17:59:48 -0300

    created useResult's hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,32 +2,19 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term,
-          location: "porto alegre",
-        },
-      });
-
-      setResults(response.data.businesses);
-    } catch (e) {
-      setErrorMessage("Something went wrong");
-    }
-  };
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View style={styles.background}>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
