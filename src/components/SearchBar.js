b7fbdbc0a1adf87841052fa3be6476c17861bf7e
Pedro <pedro.nunes@southsystem.com.br>
2020-12-12 17:21:50 -0300

    added api call to search restaurant

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,7 @@ const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
         placeholder="Search"
         value={term}
         onChangeText={(newTerm) => onTermChange(newTerm)}
-        onEndEditing={() => {}}
+        onEndEditing={() => onTermSubmit()}
       />
     </View>
   );
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,19 +2,34 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "porto alegre",
+        },
+      });
+
+      setResults(response.data.businesses);
+    } catch (e) {
+      setErrorMessage("Something went wrong");
+    }
+  };
 
   return (
     <View style={styles.background}>
-      <SearchBar
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => {}}
-      />
-      <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
